ac5df57532ccd1a75e14d588d8c77f80da9e6468
barkode <konstantinks@gmail.com>
2024-02-18 15:12:39 +0000

    Add Cart page

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+import { Wrapper } from './CartPage.styled';
+import { PromoComponent } from 'components/PromoComponent/PromoComponent';
+import { UserCartComponent } from 'components/UserCartComponent/UserCartComponent';
+
 export const CartPage = () => {
-  return <div>CartPage</div>;
+  return (
+    <Wrapper>
+      <UserCartComponent />
+      <PromoComponent />
+    </Wrapper>
+  );
 };
--- src/pages/CartPage/CartPage.styled.jsx
+++ src/pages/CartPage/CartPage.styled.jsx
@@ -1,1 +1,7 @@
 import styled from 'styled-components';
+
+export const Wrapper = styled.section`
+  padding: 200px 120px 100px 120px;
+  max-width: 1440px;
+  margin: 0 auto;
+`;
