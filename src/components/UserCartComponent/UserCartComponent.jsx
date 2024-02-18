a953685b472524dde1c7a268d5a4bc86813005ce
barkode <konstantinks@gmail.com>
2024-02-18 15:12:51 +0000

    add User Cart component

---
+++ src/components/UserCartComponent/UserCartComponent.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Wrapper } from './UserCartComponent.styled';
+
+export const UserCartComponent = () => {
+  return <Wrapper>UserCartComponent</Wrapper>;
+};
---
+++ src/components/UserCartComponent/UserCartComponent.styled.jsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  width: 792px;
+  height: 416px;
+  background-color: grey;
+`;
