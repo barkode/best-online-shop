dfbaef785d36bd3647a634f485538f08628b6038
barkode <konstantinks@gmail.com>
2024-02-18 21:41:13 +0000

    update element

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
-import { Wrapper } from './CartPage.styled';
+import { Container, Wrapper } from './CartPage.styled';
 import { PromoComponent } from 'components/PromoComponent/PromoComponent';
 import { UserCartComponent } from 'components/UserCartComponent/UserCartComponent';
 
 export const CartPage = () => {
   return (
     <Wrapper>
-      <UserCartComponent />
-      <PromoComponent />
+      <Container>
+        <UserCartComponent />
+        <PromoComponent />
+      </Container>
     </Wrapper>
   );
 };
--- src/pages/CartPage/CartPage.styled.jsx
+++ src/pages/CartPage/CartPage.styled.jsx
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.section`
+  width: 100%;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
   padding: 200px 120px 100px 120px;
   max-width: 1440px;
   margin: 0 auto;
